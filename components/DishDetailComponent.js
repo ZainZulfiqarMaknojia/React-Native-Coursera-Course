fd68e7c64e65ee683cd6aaa18c9c7fce70804c74
Zain Zulfiqar <zainzulfiqarmaknojia@gmail.com>
2020-07-22 21:52:28 +0500

    navigation using react stack navigator

--- components/DishDetailComponent.js
+++ components/DishDetailComponent.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React ,{ Component } from 'react';
 import { Text, View } from 'react-native';
 import { Card } from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
 
-function Dishdetail(props) {
-    return(<RenderDish dish={props.dish} />);
+
+class Dishdetail extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES
+        };
+    }
+
+    static navigationOptions = {
+        title: 'Dish Details'
+    };
+
+    render() {
+        const dishId = this.props.navigation.getParam('dishId','');
+        return(<RenderDish dish={this.state.dishes[+dishId]} />);
+    }
 }
 
 function RenderDish(props) {
